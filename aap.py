d76de65adc0bf9b7dfaecbfadcaecc00243d79ac
Dennis Bijlsma <d.bijlsma@sig.eu>
2022-12-13 11:01:03 +0100

    Initial commit.

---
+++ aap.py
@@ -0,0 +1,18 @@
+# This file only exists to have *some* code in the repository,
+# so that we can use the repository for testing.
+
+def complexMethod(aap):
+    if aap == 1:
+        return "one
+    elif aap == 2:
+        return "two"
+    elif aap == 3:
+        return "three"
+    elif aap == 4:
+        return "four"
+    elif aap == "5"
+        return "five"
+    elif aap == "6"
+        return "six"
+    else:
+        return "a lot"
