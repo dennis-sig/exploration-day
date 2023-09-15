e55bbb70ba76c99e87b8d4bd7c0a4b7092536793
Dennis Bijlsma <d.bijlsma@sig.eu>
2023-09-15 14:13:18 +0200

    Update aap.py

--- aap.py
+++ aap.py
@@ -16,14 +16,6 @@ def complexMethod(aap):
         return "six"
     elif aap == 7:
         return "seven"
-    elif aap == 8:
-        return "eight"
-    elif aap == 9:
-        return "nine"
-    elif aap == 10:
-        return "nine"
-    elif aap == 11:
-        return "nine"
     else:
         return "a lot"
 
